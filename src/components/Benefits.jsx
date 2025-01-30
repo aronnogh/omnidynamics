b26d1f15499f566e0355147515b17051993b2f1c
aronnogh <koushik.647433@gmail.com>
2025-01-31 04:14:40 +0530

    Update Benefits.jsx

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -17,36 +17,16 @@ const Benefits = () => {
         <div className="flex flex-wrap gap-10 mb-10">
           {benefits.map((item) => (
             <div
-              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
-              style={{
-                backgroundImage: `url(${item.backgroundUrl})`,
-              }}
+              className="relative rounded-[2.5rem] p-[1px] bg-gradient-to-b from-[#1C1C1D] via-[#131313] to-[#0C0C0C] hover:bg-gradient-to-b hover:from-[#2C2C2D] hover:via-[#1C1C1D] hover:to-[#131313] transition-all duration-300 md:max-w-[24rem] group"
               key={item.id}
             >
-              <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
+              <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] rounded-[2.4rem] bg-n-8 overflow-hidden">
+                <div className="absolute inset-0 bg-gradient-to-b from-[rgba(255,255,255,0.02)] to-transparent pointer-events-none" />
+                
                 <h5 className="h5 mb-5">{item.title}</h5>
                 <p className="body-2 mb-6 text-n-3">{item.text}</p>
-                {/* <div className="flex items-center mt-auto">
-                  <img
-                    src={item.iconUrl}
-                    width={48}
-                    height={48}
-                    alt={item.title}
-                  />
-                  <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider">
-                    Explore more
-                  </p>
-                  <Arrow />
-                </div> */}
-              </div>
 
-              {item.light && <GradientLight />}
-
-              <div
-                className="absolute inset-0.5 bg-n-8"
-                style={{ clipPath: "url(#benefits)" }}
-              >
-                <div className="absolute inset-0 opacity-0 transition-opacity hover:opacity-10">
+                <div className="absolute inset-0.5 bg-n-8 opacity-0 group-hover:opacity-10 transition-opacity">
                   {item.imageUrl && (
                     <img
                       src={item.imageUrl}
@@ -59,7 +39,7 @@ const Benefits = () => {
                 </div>
               </div>
 
-              <ClipPath />
+              <div className="absolute -z-10 inset-0 bg-gradient-to-b from-[rgba(255,255,255,0.1)] to-transparent blur-xl opacity-0 group-hover:opacity-100 transition-opacity" />
             </div>
           ))}
         </div>
